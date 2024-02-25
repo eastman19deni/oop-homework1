2b173e3cd6ff1f370e25b68c43f71b20ed1dee6b
Daniyal Ganyatov <deni12349075@gmail.com>
2024-02-26 01:07:05 +0200

    lil bit fixing but not important i think nothing changed

--- Characters/Sniper.java
+++ Characters/Sniper.java
@@ -1,64 +1,28 @@
 
 import java.util.ArrayList;
-import java.util.List;
 
 
-public class Sniper extends BaseCharacter implements Bows{
-    int bullets;
+
+public class Sniper extends Bows{    
+
     public Sniper(String name, Integer x, Integer y) {
-        super(1, name, 60, 50, 40, 20, true, x, y);
-        this.bullets = bullets;
+        super(name, x, y);
+        this.arrows = 5;
     }
 
     @Override
-    public String toString() {
-        return String.format("Class: %s  Name: %s", this.getClass().getSimpleName(), getName());
-    }
-
-    @Override
-    public void attack(BaseCharacter target) {
-        super.attack(target);
-    }
-
-    @Override
-    public void death(BaseCharacter target) {
-        super.death(target);
-    }
-
-    @Override
-    public void getDamage(int damage) {
-        super.getDamage(damage);
-    }
-
-    @Override
-    protected double getDistanceTo(BaseCharacter other) {
-        return super.getDistanceTo(other);
-    }
-
-    @Override
-    protected int getHealth() {
-        return super.getHealth();
-    }
-
-    @Override
-    public String getInfo() {
-        return super.getInfo();
-    }
-
-    @Override
-    public BaseCharacter nearestEnemy(List<BaseCharacter> targets) {
-        return super.nearestEnemy(targets);
-    }
-
-    @Override
-    public void GetDamage(BaseCharacter target, int damage) {
+    public void death() {
+        if (this.getHealth() < 1) {
+            System.out.println("Неточность - вежливость снайперов и это моя последняя...");
+        }
     }
 
     @Override
     public void step(ArrayList<BaseCharacter> enemy) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'step'");
-    }
+    }    
+
 
     
 }
