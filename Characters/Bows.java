3fbfa9af2d990faeea44f9432deffda37f0874a4
Daniyal Ganyatov <deni12349075@gmail.com>
2024-02-26 01:05:57 +0200

    add step for corecting getting information

--- Characters/Bows.java
+++ Characters/Bows.java
@@ -1,9 +1,33 @@
+import java.util.List;
+abstract public class Bows extends BaseCharacter {
 
-public interface Bows {
+    public Bows(String name, int x, int y) {
+        super(name, x, y);
+        this.stamina = 3;
+
+    }
+
+    protected Integer arrows;
     
-    void attack (BaseCharacter target);
+    public Integer getArrows() {return arrows;}
 
-    void GetDamage(BaseCharacter target, int damage);
+    public void setArrows(Integer arrows) {this.arrows = arrows;}
+ 
 
-    void death(BaseCharacter target);
+    public void attac(BaseCharacter target){
+        int damage = r.nextInt(5);
+        this.arrows--;
+        target.getDamage(damage);
+    }
+
+    public void step(List<BaseCharacter> list, List<BaseCharacter> friends) {
+
+        if (!isDead() || getArrows() <= 0) return;
+            attac(nearestEnemy(list));
+            System.out.println(toString());
+
+    }
+        
+    
+    
 }
