6a4053e127b739c10e4baf1c5c6d196344ed2140
Daniyal Ganyatov <deni12349075@gmail.com>
2024-02-29 00:01:22 +0200

    deleted useless components

--- Characters/MyInterface.java
+++ Characters/MyInterface.java
@@ -2,13 +2,5 @@
 import java.util.List;
 
 public interface MyInterface {
-    String enemy = null;
-
-    BaseCharacter target = nearestEnemy(enemy);
-    static BaseCharacter nearestEnemy(String enemy2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'nearestEnemy'");
-    }
     void step(List<BaseCharacter> list,List<BaseCharacter> friends);
-    
 }
